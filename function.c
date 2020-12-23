f302f2d2d71de9a54f3ade3adc5a853a12f82dc8
Antioch Jeanett <ajeanett@et-e3.kzn.21-school.ru>
2020-12-23 21:01:39 +0300

    проверил по норме

--- function.c
+++ function.c
@@ -6,7 +6,7 @@
 /*   By: ajeanett <ajeanett@21-school.ru>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 19:20:19 by ajeanett          #+#    #+#             */
-/*   Updated: 2020/12/23 19:22:07 by ajeanett         ###   ########.fr       */
+/*   Updated: 2020/12/23 21:00:48 by ajeanett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@ int		ft_isspace(int c)
 	return (0);
 }
 
-void    ft_free(char **str)
+void	ft_free(char **str)
 {
-    if (*str)
-	    free(*str);
+	if (*str)
+		free(*str);
 	*str = NULL;
 }
 
@@ -31,9 +31,9 @@ char	*ft_chrjoin(char *s1, const char s2)
 {
 	unsigned int	buf;
 	char			*str;
-    int             i;
-    
-    i = 0;
+	int				i;
+
+	i = 0;
 	buf = ft_strlen(s1) + 1;
 	str = ((char *)malloc(sizeof(char) * (buf + 1)));
 	if (!str)
@@ -41,7 +41,7 @@ char	*ft_chrjoin(char *s1, const char s2)
 	while (s1[i] != '\0')
 	{
 		str[i] = s1[i];
-        i++;
+		i++;
 	}
 	if (s2 != '\0')
 		str[i++] = s2;
@@ -60,15 +60,15 @@ int		line_to_arg(char **line, char chr)
 	return (0);
 }
 
-void    check_arg(t_all *all)
+void	check_arg(t_all *all)
 {
-    if (all->name_parsed == 2)
-    {
-        all->name_parsed = 1;
-        all->count++;
-        all->size++;
-        all->arg = ft_realloc(all->arg, all->size);
-        all->arg[all->count] = ft_strdup("");
-        all->arg[all->count + 1] = NULL;
-    }
+	if (all->name_parsed == 2)
+	{
+		all->name_parsed = 1;
+		all->count++;
+		all->size++;
+		all->arg = ft_realloc(all->arg, all->size);
+		all->arg[all->count] = ft_strdup("");
+		all->arg[all->count + 1] = NULL;
+	}
 }
